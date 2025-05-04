8f78f39b2a4d36a9a81624290368eac4eaf94f92
YuriyNik156 <yuriy.nik156@gmail.com>
2025-05-04 15:20:05 +0300

    create hometask_find_in_vikipedia.py and update content.py

--- content.py
+++ content.py
@@ -1,10 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+import random
 import time
 
 browser = webdriver.Chrome()
 browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
 
-input("Нажмите Enter, чтобы завершить скрипт")
+hatnotes = []
+
+for element in browser.find_elements(By.TAG_NAME, "div"):
+    cl = element.get_attribute("class")
+    if cl == "hatnote navigation-not-searchable ts-main":
+        hatnotes.append(element)
+
+print(hatnotes)
+hatnote = random.choice(hatnotes)
+link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+browser.get(link)
+
+input("Нажмите Enter для завершения сеанса")
